6c62316aafdade5738e8945fe3baed58f8b9b972
Lars Elgtvedt Susaas <larssusaas@gmail.com>
2025-01-04 15:28:06 +0100

    adding subobject view

--- EDSEditorGUI2/Views/DeviceODView.axaml.cs
+++ EDSEditorGUI2/Views/DeviceODView.axaml.cs
@@ -2,19 +2,29 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using DialogHostAvalonia;
 using EDSEditorGUI2.Converter;
+using LibCanOpen;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace EDSEditorGUI2.Views;
 
 public partial class DeviceODView : UserControl
 {
+    private List<DataGrid> _odViews = [];
     public DeviceODView()
     {
         InitializeComponent();
 
-        var values = Enum.GetNames(typeof(LibCanOpen.OdObject.Types.ObjectType)).Skip(1).ToArray();
+        var values = Enum.GetNames(typeof(OdObject.Types.ObjectType)).Skip(1).ToArray();
         type.ItemsSource = values;
+        ODView_Com.grid.SelectionChanged += GridSelectionChanged;
+        ODView_Manufacture.grid.SelectionChanged += GridSelectionChanged;
+        ODView_Device.grid.SelectionChanged += GridSelectionChanged;
+
+        _odViews.Add(ODView_Com.grid);
+        _odViews.Add(ODView_Manufacture.grid);
+        _odViews.Add(ODView_Device.grid);
     }
     private void OnDialogClosing(object? sender, DialogClosingEventArgs e)
     {
@@ -26,12 +36,27 @@ public partial class DeviceODView : UserControl
             }
         }
     }
+
+    private void GridSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if(sender is DataGrid s && DataContext is ViewModels.DeviceOD dc)
+        {
+            if(s.SelectedItem is KeyValuePair<string, OdObject> selected)
+            {
+                dc.SelectedObject = selected.Value;
+                foreach (var dg in _odViews)
+                {
+                    if (dg != s)
+                    {
+                        dg.SelectedItem = null;
+                    }
+                }
+            }
+        }
+    }
+
     private async void AddIndex(object? sender, RoutedEventArgs e)
     {
         await DialogHost.Show(Resources["NewIndexDialog"]!, "NoAnimationDialogHost");
-    }
-
-    private void DataGrid_SelectionChanged(object? sender, Avalonia.Controls.SelectionChangedEventArgs e)
-    {
     }
 }
